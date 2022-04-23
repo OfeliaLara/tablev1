45e5529348cba8356a2d395f21f5b74ffe88ee47
Ofelia Lara <104162780+OfeliaLara@users.noreply.github.com>
2022-04-23 20:58:55 +0000

    Finished Table

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from tabulate import tabulate
-
-print(tabulate([["1. Fettuccine Alfredo", "  $16.99"], ["2. Chicken Alfredo", "$20.99"], ["3. Shrimp Scampi", "$21.99"]],   ["Pasta" , "  $", ], "fancy_grid"))
-
-'''next line of code is for the table format in case i delete it again...
-print(tabulate([["1. Fettuccine Alfredo", "  $16.99"], ["2. Chicken Alfredo", "$20.99"], ["3. Shrimp Scampi", "21.99"]],   ["Pasta" , "  $", ], "fancy_grid"))
 '''
-print(tabulate([["1. Zuppa Toscana", "  $8.99"], ["2. Chicken & Gnocchi", "$9.99"], ["3. Pasta e Fagioli", "$7.99"]],   ["Soups" , "  $", ], "fancy_grid")) 
+print(tabulate([["1. Fettuccine Alfredo", "  $16.99", "4. Zuppa Toscana", "  $7.99", "7. Fountain Drinks", "$2.00"], ["2. Chicken Alfredo", "$20.99", "5. Chicken Gnocchi", "  $8.99", "8. Sweet Ice Tea", "$3.50"], ["3. Shrimp Scampa", "$21.99", "6. Pasta e Fagioli", "$9.99", "9. Regular Lemonade", "$3.99"]],   ["Pasta" , "  $", "Soups" , "  $", "Drinks", "  $" ], "fancy_grid"))
+'''
 
-print(tabulate([["spam", 41.9999], ["eggs", "451.0"]],                 ["strings", "numbers"], "fancy_grid"))
+
