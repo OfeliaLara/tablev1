c20486ffc578c5354b09e132c47cdcc837ba4858
Ofelia Lara <104162780+OfeliaLara@users.noreply.github.com>
2022-04-24 00:18:05 +0000

    Conditional based off user input

--- main.py
+++ main.py
@@ -2,5 +2,13 @@ from tabulate import tabulate
 '''
 print(tabulate([["1. Fettuccine Alfredo", "  $16.99", "4. Zuppa Toscana", "  $7.99", "7. Fountain Drinks", "$2.00"], ["2. Chicken Alfredo", "$20.99", "5. Chicken Gnocchi", "  $8.99", "8. Sweet Ice Tea", "$3.50"], ["3. Shrimp Scampa", "$21.99", "6. Pasta e Fagioli", "$9.99", "9. Regular Lemonade", "$3.99"]],   ["Pasta" , "  $", "Soups" , "  $", "Drinks", "  $" ], "fancy_grid"))
 '''
+'''
+Friend_Q1= input('What is your name? ')
+Friend_Q2= input('What is your favorite show? ')
+Friend_Q3 = input('Who is your favorite artist? ')
 
-
+if ("I don't know" in Friend_Q1 or "I don't know" in Friend_Q2 or "I don't know" in Friend_Q3): 
+  print("You don't seem like a nice person...")
+else: 
+  print ("You seem like an awesome person, " + Friend_Q1 +"!")
+'''
